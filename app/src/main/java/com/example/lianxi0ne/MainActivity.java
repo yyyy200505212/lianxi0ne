209110e824f1ea28b7d4b3b3032720a2d11e6958
yyyy200505212 <1535154819@qq.com>
2022-04-29 10:02:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lianxi0ne/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lianxi0ne;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
